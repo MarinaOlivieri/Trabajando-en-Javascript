70837beecf1072d697d4aff381542007f5c3995f
Marina Olivieri <mari_debenedetti@hotmail.com>
2022-06-14 21:20:25 -0300

    Utilizando Arrays

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,14 +1,49 @@
-function solicitarNumero(){
-    let numero = prompt("Ingrese un numero");
-    return parseInt(numero)
-} 
-function sumar(parametroA, parametroB){
-    let resultado = parametroA + parametroB
-    console.log("resultado es:" + resultado)
-    return resultado
+/* Entrega opcinal numero uno*/
+
+/*CREANDO TABLA DE MULTIPLICAR*/
+
+// let numero = 9
+// for (let index = 1; index <=10; index++){
+//     let resultado = index * numero
+//     console.log("Resultado de 9 * " + index + " = " + resultado)
+// }
+
+/*TRABAJANDO CON CICLOS*/
+
+// let entrada = prompt("Ingresa tu nombre")
+// while(entrada !== "Santiago"){
+//     entrada = prompt("Ingrese nuevamente su nombre")
+// }
+
+/* =======================================================*/
+
+/*Desafio numero uno*/
+
+/*TRABAJANDO CON FUNCIONES*/
+
+// function solicitarNumero(){
+//     let numero = prompt("Ingrese un numero");
+//     return parseInt(numero)
+// } 
+// function sumar(parametroA, parametroB){
+//     let resultado = parametroA + parametroB
+//     console.log("resultado es:" + resultado)
+//     return resultado
+// }
+
+// let a = solicitarNumero()
+// let b = solicitarNumero()
+// let sum = sumar(a, b)
+// console.log(sum)
+
+/*========================================================*/
+
+/*Entregable opcional numero dos*/
+
+/*TRABAJANDO CON LISTAS*/
+
+const lista = ['Angelina', 'Aaron', 'Nicolas', 'Eloy', 'Pablo', 'Marina', 'Gabriela','Walter']
+for (let index = 0; index < 8; index++) {
+    console.log('Nombre: ' + lista[index])    
 }
 
-let a = solicitarNumero()
-let b = solicitarNumero()
-let sum = sumar(a, b)
-console.log(sum)
